8dcbc674d21286a98fd4f5a4a64184b767a746e0
Kristina <yarevyakinakristina@gmail.com>
2023-03-02 15:01:42 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    /* плавная прокрутка меню */
+    $('nav a[href^="#"]').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top //скролл сверху вниз
+    }, 1000);
+    /* добавить/удалить класс active */
+    $('nav a[href^="#"]').parent().removeClass('active');
+    $(this).parent().addClass('active');
+    $('.menu').toggle(500);
+    $('.menu__burger').toggleClass('close');
+    return false;
+    });
+    /* мобильное меню */
+    $('.menu__burger').click(function(){
+        $('.menu').toggle(500);
+        $(this).toggleClass('close');
+    });
+});
